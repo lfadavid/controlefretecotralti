8b334856367f0db78eb42b0ebdd2590328c72964
lfadavid <167301084+lfadavid@users.noreply.github.com>
2024-11-19 09:35:01 -0300

    Update calculadorafrete.py

--- calculadorafrete.py
+++ calculadorafrete.py
@@ -105,7 +105,7 @@ if peso_digitado is not None and rota_digitada is not None:
         st.write("❶ Para calcular o **FRETE TOTAL** é necessário somar: ( **Frete Rota + Taxa NF + ADValorem + Icms )**.")
         st.write(f"❷ O Destino é as  **{regiao}** .")
         st.write(f"❸ O valor da Taxa de emissão do CTE é de **R$ {taxanf:.2f} reais**.")
-        st.write(f"❹ O **AD VALOREM** (_é uma taxa cobrada pelo transportador para proteger a carga contra danos durante a movimentação_) o valor é de  **{adv:,.3f}%**.")
+        st.write(f"❹ O **AD VALOREM** (_é uma taxa cobrada pelo transportador para proteger a carga contra danos durante a movimentação_) o valor é de **0,05%**.")
         st.write(f"❺ O **ICMS** de transporte (_é o imposto que incide sobre o serviço de transporte de cargas, seja ele rodoviário, ferroviário, aéreo ou aquaviário_)  **RJ x RJ = 22%** , **RJ x _( SP, MG, PR, SC e RS )_ = 12%** .")
         st.write(f"❻ O Tipo da Carga é **{tipo}**.")
         st.divider()
